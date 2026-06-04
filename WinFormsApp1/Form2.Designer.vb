135e2a69dfdbf9e4d3af053ca6c97b8718bf7310
Aisha2mohammed <hayunadus111@gmail.com>
2026-06-04 10:00:00 +0300

    feat: add Form2 main logic and designer

---
+++ WinFormsApp1/Form2.Designer.vb
@@ -0,0 +1,175 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form2
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Label1 = New Label()
+        Label2 = New Label()
+        Label4 = New Label()
+        Label5 = New Label()
+        TextBox2 = New TextBox()
+        TextBox3 = New TextBox()
+        Label7 = New Label()
+        LinkLabel1 = New LinkLabel()
+        Button1 = New Button()
+        TextBox1 = New TextBox()
+        Label3 = New Label()
+        SuspendLayout()
+        ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.Location = New Point(744, 438)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(53, 20)
+        Label1.TabIndex = 0
+        Label1.Text = "Label1"
+        ' 
+        ' Label2
+        ' 
+        Label2.AutoSize = True
+        Label2.Font = New Font("Segoe UI", 10.2F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        Label2.Location = New Point(147, 54)
+        Label2.Name = "Label2"
+        Label2.Size = New Size(87, 23)
+        Label2.TabIndex = 1
+        Label2.Text = "REGISTER"
+        ' 
+        ' Label4
+        ' 
+        Label4.AutoSize = True
+        Label4.Font = New Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        Label4.Location = New Point(147, 175)
+        Label4.Name = "Label4"
+        Label4.Size = New Size(91, 20)
+        Label4.TabIndex = 3
+        Label4.Text = "USERNAME"
+        ' 
+        ' Label5
+        ' 
+        Label5.AutoSize = True
+        Label5.Font = New Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        Label5.Location = New Point(147, 226)
+        Label5.Name = "Label5"
+        Label5.Size = New Size(91, 20)
+        Label5.TabIndex = 4
+        Label5.Text = "PASSWORD"
+        ' 
+        ' TextBox2
+        ' 
+        TextBox2.Location = New Point(272, 175)
+        TextBox2.Name = "TextBox2"
+        TextBox2.Size = New Size(276, 27)
+        TextBox2.TabIndex = 7
+        ' 
+        ' TextBox3
+        ' 
+        TextBox3.Location = New Point(272, 226)
+        TextBox3.Name = "TextBox3"
+        TextBox3.Size = New Size(276, 27)
+        TextBox3.TabIndex = 8
+        ' 
+        ' Label7
+        ' 
+        Label7.AutoSize = True
+        Label7.Location = New Point(183, 359)
+        Label7.Name = "Label7"
+        Label7.Size = New Size(150, 20)
+        Label7.TabIndex = 10
+        Label7.Text = "Have you logged in? "
+        ' 
+        ' LinkLabel1
+        ' 
+        LinkLabel1.AutoSize = True
+        LinkLabel1.LinkColor = Color.Black
+        LinkLabel1.Location = New Point(353, 359)
+        LinkLabel1.Name = "LinkLabel1"
+        LinkLabel1.Size = New Size(43, 20)
+        LinkLabel1.TabIndex = 11
+        LinkLabel1.TabStop = True
+        LinkLabel1.Text = "login"
+        ' 
+        ' Button1
+        ' 
+        Button1.BackColor = Color.Black
+        Button1.Font = New Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        Button1.ForeColor = Color.White
+        Button1.Location = New Point(147, 287)
+        Button1.Name = "Button1"
+        Button1.Size = New Size(401, 46)
+        Button1.TabIndex = 12
+        Button1.Text = "REGISTER"
+        Button1.UseVisualStyleBackColor = False
+        ' 
+        ' TextBox1
+        ' 
+        TextBox1.Location = New Point(272, 125)
+        TextBox1.Name = "TextBox1"
+        TextBox1.Size = New Size(276, 27)
+        TextBox1.TabIndex = 13
+        ' 
+        ' Label3
+        ' 
+        Label3.AutoSize = True
+        Label3.Font = New Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        Label3.Location = New Point(147, 128)
+        Label3.Name = "Label3"
+        Label3.Size = New Size(80, 20)
+        Label3.TabIndex = 14
+        Label3.Text = "Full Name"
+        ' 
+        ' Form2
+        ' 
+        AutoScaleDimensions = New SizeF(8F, 20F)
+        AutoScaleMode = AutoScaleMode.Font
+        BackColor = Color.White
+        ClientSize = New Size(706, 450)
+        Controls.Add(Label3)
+        Controls.Add(TextBox1)
+        Controls.Add(Button1)
+        Controls.Add(LinkLabel1)
+        Controls.Add(Label7)
+        Controls.Add(TextBox3)
+        Controls.Add(TextBox2)
+        Controls.Add(Label5)
+        Controls.Add(Label4)
+        Controls.Add(Label2)
+        Controls.Add(Label1)
+        FormBorderStyle = FormBorderStyle.FixedToolWindow
+        Name = "Form2"
+        Text = "Form2"
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents TextBox3 As TextBox
+    Friend WithEvents Label7 As Label
+    Friend WithEvents LinkLabel1 As LinkLabel
+    Friend WithEvents Button1 As Button
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents Label3 As Label
+End Class
