8c67d1b157f11528ba58840f76308bfedf7fd70b
Mosisa Boneya <135672652+Mosisaboneya4@users.noreply.github.com>
2026-06-07 15:59:24 +0300

    Add files via upload
    ADDED THE FORM 1

---
+++ WinFormsApp1/Form1.Designer.vb
@@ -0,0 +1,172 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        TextBox1 = New TextBox()
+        TextBox2 = New TextBox()
+        Label1 = New Label()
+        Label2 = New Label()
+        Label3 = New Label()
+        Button1 = New Button()
+        Label4 = New Label()
+        Label5 = New Label()
+        LinkLabel1 = New LinkLabel()
+        SuspendLayout()
+        ' 
+        ' TextBox1
+        ' 
+        TextBox1.BackColor = Color.White
+        TextBox1.Location = New Point(159, 184)
+        TextBox1.Name = "TextBox1"
+        TextBox1.Size = New Size(303, 27)
+        TextBox1.TabIndex = 0
+        ' 
+        ' TextBox2
+        ' 
+        TextBox2.BackColor = Color.White
+        TextBox2.Location = New Point(159, 262)
+        TextBox2.Name = "TextBox2"
+        TextBox2.Size = New Size(303, 27)
+        TextBox2.TabIndex = 1
+        ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.Font = New Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        Label1.ForeColor = Color.Black
+        Label1.Location = New Point(159, 143)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(91, 20)
+        Label1.TabIndex = 2
+        Label1.Text = "USERNAME"
+        ' 
+        ' Label2
+        ' 
+        Label2.AutoSize = True
+        Label2.Font = New Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        Label2.ForeColor = Color.Black
+        Label2.Location = New Point(158, 224)
+        Label2.Name = "Label2"
+        Label2.Size = New Size(91, 20)
+        Label2.TabIndex = 3
+        Label2.Text = "PASSWORD"
+        ' 
+        ' Label3
+        ' 
+        Label3.AutoSize = True
+        Label3.Font = New Font("Segoe UI", 10.8F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        Label3.ForeColor = Color.Black
+        Label3.Location = New Point(159, 26)
+        Label3.Name = "Label3"
+        Label3.Size = New Size(101, 25)
+        Label3.TabIndex = 4
+        Label3.Text = "WELCOME"
+        ' 
+        ' Button1
+        ' 
+        Button1.BackColor = Color.Black
+        Button1.Font = New Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        Button1.ForeColor = Color.White
+        Button1.Location = New Point(159, 322)
+        Button1.Name = "Button1"
+        Button1.Size = New Size(303, 53)
+        Button1.TabIndex = 5
+        Button1.Text = "LOGIN"
+        Button1.UseVisualStyleBackColor = False
+        ' 
+        ' Label4
+        ' 
+        Label4.AutoSize = True
+        Label4.ForeColor = Color.Black
+        Label4.Location = New Point(159, 80)
+        Label4.Name = "Label4"
+        Label4.Size = New Size(278, 20)
+        Label4.TabIndex = 6
+        Label4.Text = "Welcome back! Please enter your details."
+        ' 
+        ' Label5
+        ' 
+        Label5.AutoSize = True
+        Label5.Location = New Point(168, 399)
+        Label5.Name = "Label5"
+        Label5.Size = New Size(138, 20)
+        Label5.TabIndex = 7
+        Label5.Text = "Are you registered?"
+        ' 
+        ' LinkLabel1
+        ' 
+        LinkLabel1.AutoSize = True
+        LinkLabel1.LinkColor = Color.Black
+        LinkLabel1.Location = New Point(312, 399)
+        LinkLabel1.Name = "LinkLabel1"
+        LinkLabel1.Size = New Size(63, 20)
+        LinkLabel1.TabIndex = 8
+        LinkLabel1.TabStop = True
+        LinkLabel1.Text = "Register"
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(8F, 20F)
+        AutoScaleMode = AutoScaleMode.Font
+        BackColor = Color.White
+        ClientSize = New Size(607, 486)
+        Controls.Add(LinkLabel1)
+        Controls.Add(Label5)
+        Controls.Add(Label4)
+        Controls.Add(Button1)
+        Controls.Add(Label3)
+        Controls.Add(Label2)
+        Controls.Add(Label1)
+        Controls.Add(TextBox2)
+        Controls.Add(TextBox1)
+        FormBorderStyle = FormBorderStyle.FixedToolWindow
+        Name = "Form1"
+        Text = "Form1"
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Button1 As Button
+
+    Private Sub Label3_Click(sender As Object, e As EventArgs) Handles Label3.Click
+
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Friend WithEvents Label4 As Label
+
+    Private Sub Label4_Click(sender As Object, e As EventArgs) Handles Label4.Click
+
+    End Sub
+
+    Friend WithEvents Label5 As Label
+    Friend WithEvents LinkLabel1 As LinkLabel
+End Class
---
+++ WinFormsApp1/Form1.vb
@@ -0,0 +1,70 @@
+﻿Imports MySql.Data.MySqlClient
+
+Public Class Form1
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim username As String = TextBox1.Text.Trim()
+        Dim password As String = TextBox2.Text.Trim()
+
+        If username = "" Or password = "" Then
+            MessageBox.Show("Please enter username and password")
+            Exit Sub
+        End If
+
+        Dim connString As String = "server=localhost;user id=root;password=;database=login_db"
+
+        Try
+            Using conn As New MySqlConnection(connString)
+                conn.Open()
+
+                Dim query As String = "SELECT role FROM users WHERE username=@user AND password=@pass"
+                Using cmd As New MySqlCommand(query, conn)
+
+                    cmd.Parameters.AddWithValue("@user", username)
+                    cmd.Parameters.AddWithValue("@pass", password)
+
+                    Dim result = cmd.ExecuteScalar()
+
+                    If result IsNot Nothing Then
+                        Dim role As String = result.ToString().ToLower()
+
+                        If role = "admin" Then
+                            MessageBox.Show("Welcome Admin!")
+
+                            Dim adminForm As New Form3()
+                            adminForm.loggedUsername = username
+                            adminForm.Show()
+                            Me.Hide()
+
+                        ElseIf role = "user" Then
+                            MessageBox.Show("Welcome User!")
+
+                            ' 🔥 OPEN FORM5 INSTEAD
+                            Dim f As New Form5()
+                            f.loggedUser = username   ' 🔥 PASS USERNAME
+                            f.Show()
+                            Me.Hide()
+
+                        Else
+                            MessageBox.Show("Unknown role detected!")
+                        End If
+
+                    Else
+                        MessageBox.Show("Invalid Username or Password")
+                    End If
+
+                End Using
+            End Using
+
+        Catch ex As Exception
+            MessageBox.Show("Error: " & ex.Message)
+        End Try
+    End Sub
+
+    Private Sub LinkLabel1_LinkClicked(sender As Object, e As LinkLabelLinkClickedEventArgs) Handles LinkLabel1.LinkClicked
+        Dim registerForm As New Form2()
+        registerForm.Show()
+        Me.Hide()
+    End Sub
+
+End Class
